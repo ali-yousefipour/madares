389cfea0d8f26c0a95908e99929cb191924e6d7f
علی یوسفی پور <ali.yousefipour@gmail.com>
2026-09-22 11:33:32 +0330

    همگام‌سازی کتابخانه فعال سایت: app/lib/Xlsx.php

--- app/lib/Xlsx.php
+++ app/lib/Xlsx.php
@@ -24,9 +24,38 @@ class Xlsx {
     return $n - 1;
   }
   public static function eachRow($path, callable $cb) {
+    self::eachRowIn($path, 'xl/worksheets/sheet1.xml', $cb);
+  }
+  // فهرست شیت‌های فایل اکسل به ترتیب همان‌طور که در ورک‌بوک تعریف شده‌اند: [['name'=>..,'target'=>'xl/worksheets/sheetX.xml'], ...]
+  public static function listSheets($path) {
+    $sheets = [];
+    $wbXml = @file_get_contents('zip://' . $path . '#xl/workbook.xml');
+    $relsXml = @file_get_contents('zip://' . $path . '#xl/_rels/workbook.xml.rels');
+    if ($wbXml === false) return $sheets;
+    $wb = @simplexml_load_string($wbXml);
+    if (!$wb || !isset($wb->sheets->sheet)) return $sheets;
+    $relMap = [];
+    if ($relsXml !== false) {
+      $rels = @simplexml_load_string($relsXml);
+      if ($rels) foreach ($rels->Relationship as $rel) { $relMap[(string)$rel['Id']] = (string)$rel['Target']; }
+    }
+    $rNs = 'http://schemas.openxmlformats.org/officeDocument/2006/relationships';
+    $i = 0;
+    foreach ($wb->sheets->sheet as $sheet) {
+      $i++;
+      $rAttrs = $sheet->attributes($rNs);
+      $rid = (string)($rAttrs['id'] ?? '');
+      $target = $relMap[$rid] ?? ('worksheets/sheet' . $i . '.xml');
+      $target = ltrim($target, '/');
+      if (strpos($target, 'xl/') !== 0) $target = 'xl/' . $target;
+      $sheets[] = ['name' => (string)$sheet['name'], 'target' => $target];
+    }
+    return $sheets;
+  }
+  public static function eachRowIn($path, $target, callable $cb) {
     $shared = self::sharedStrings($path);
     $r = new XMLReader();
-    if (@$r->open('zip://' . $path . '#xl/worksheets/sheet1.xml') === false) throw new Exception('شیت کاری در فایل یافت نشد');
+    if (@$r->open('zip://' . $path . '#' . $target) === false) throw new Exception('شیت کاری در فایل یافت نشد');
     $idx = 0;
     while ($r->read()) {
       if ($r->nodeType === XMLReader::ELEMENT && $r->localName === 'row') {
